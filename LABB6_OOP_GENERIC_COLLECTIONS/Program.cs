1fd52cebc73758784ec9ba43bb2d3617eddc8e6c
yasu <mrito.fadi@hotmail.com>
2025-10-12 23:49:59 +0200

    Add project files.

---
+++ LABB6_OOP_GENERIC_COLLECTIONS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LABB6_OOP_GENERIC_COLLECTIONS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
